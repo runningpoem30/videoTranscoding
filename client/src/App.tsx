7edf4307c8bd61c6deade4f871a90a59f60c2745
runningpoem30 <aryaanandpathak30@gmail.com>
2026-01-28 00:52:13 +0530

    changes

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,13 +1,48 @@
+import React, { useState, type  ChangeEvent } from 'react';
 
+export default function App() {
+  const [file, setFile] = useState<File | null>(null);
+  const BACKEND_URL = "http://localhost:8000/upload";
 
-function App() {
+  const handleFileChange = (e: ChangeEvent<HTMLInputElement>) => {
+    if (e.target.files) setFile(e.target.files[0]);
+  };
 
+  const uploadToS3 = async () => {
+    if (!file) return alert("Please select a file first!");
+
+    try {
+      
+      const response = await fetch(BACKEND_URL, {
+        method: "POST",
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({ 
+          fileName: file.name, 
+          contentType: file.type })
+      });
+      console.log(file.type)
+
+      const { uploadUrl } = await response.json();
+
+      const s3Response = await fetch(uploadUrl, {
+        method: 'PUT', 
+        body: file, 
+        headers: { "Content-Type": file.type }
+      });
+
+      if (s3Response.ok) {
+        console.log("Upload successful! S3 is now notifying SQS...");
+      }
+    } catch (err) {
+      console.error("Upload failed:", err);
+    }
+  };
 
   return (
     <div>
-     
+      <input type="file" onChange={handleFileChange} />
+      <button onClick={uploadToS3}>Upload Video</button>
     </div>
-  )
+  );
 }
 
-export default App
