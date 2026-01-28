f3d3738af2e40f609a4fe8b30bd3cda8daed4746
runningpoem30 <aryaanandpathak30@gmail.com>
2026-01-28 21:49:53 +0530

    zip

--- server/src/handler/trigger.ts
+++ server/src/handler/trigger.ts
@@ -1,48 +1,47 @@
-import { RunTaskCommand , type RunTaskCommandInput } from "@aws-sdk/client-ecs";
+import { RunTaskCommand, type RunTaskCommandInput } from "@aws-sdk/client-ecs";
 import { ecsClient } from "../lib/s3-utils.js";
 
-export const handler = async (event : any) => {
-    try{
-        const record = event.Records[0];
-        const s3Event = JSON.parse(record);;
+export const handler = async (event: any) => {
+    try {
+        // SQS wraps the S3 event in "body" as a JSON string
+        const sqsRecord = event.Records[0];
+        const s3Event = JSON.parse(sqsRecord.body);
 
-     
-
+        // Now access the S3 event data
         const bucket = s3Event.Records[0].s3.bucket.name;
-        
         const key = decodeURIComponent(s3Event.Records[0].s3.object.key.replace(/\+/g, ' '));
         console.log(`Triggering Zylar Transcoder for: s3://${bucket}/${key}`);
 
         const input: RunTaskCommandInput = {
-                cluster: process.env.CLUSTER,
-                taskDefinition: "zylar-transcoder", 
-                launchType: "FARGATE",
-                networkConfiguration: {
-                    awsvpcConfiguration: {
-                        subnets: process.env.SUBNETS ? process.env.SUBNETS.split(',') : [],
-                        securityGroups: process.env.SECURITY_GROUPS ? process.env.SECURITY_GROUPS.split(',') : [],
-                        assignPublicIp: "ENABLED",
+            cluster: process.env.CLUSTER,
+            taskDefinition: "zylar-transcoder",
+            launchType: "FARGATE",
+            networkConfiguration: {
+                awsvpcConfiguration: {
+                    subnets: process.env.SUBNETS ? process.env.SUBNETS.split(',') : [],
+                    securityGroups: process.env.SECURITY_GROUPS ? process.env.SECURITY_GROUPS.split(',') : [],
+                    assignPublicIp: "ENABLED",
+                },
+            },
+            overrides: {
+                containerOverrides: [
+                    {
+                        name: "zylar-transcoder",
+                        environment: [
+                            { name: "S3_BUCKET", value: bucket },
+                            { name: "S3_KEY", value: key },
+                            { name: 'DEST_BUCKET', value: 'zylar-processed-videos' }
+                        ],
                     },
-                },
-                overrides: {
-                    containerOverrides: [
-                        {
-                            name: "zylar-transcoder",
-                            environment: [
-                                { name: "S3_BUCKET", value: bucket },
-                                { name: "S3_KEY", value: key },
-                                { name: 'DEST_BUCKET', value: 'zylar-processed-videos' }
-                            ],
-                        },
-                    ],
-                },
-            };
+                ],
+            },
+        };
 
-            const command = new RunTaskCommand(input);
-            await ecsClient.send(command)
+        const command = new RunTaskCommand(input);
+        await ecsClient.send(command)
     }
 
-    catch(e){
+    catch (e) {
         console.log(e)
     }
 }
--- server/src/handler/worker.ts
+++ server/src/handler/worker.ts
@@ -1,61 +1,62 @@
-import { PutObjectCommand , GetObjectCommand } from "@aws-sdk/client-s3"
-import {s3Client} from "../lib/s3-utils.js"
-import fs from "fs" ; 
+import { PutObjectCommand, GetObjectCommand } from "@aws-sdk/client-s3"
+import { s3Client } from "../lib/s3-utils.js"
+import fs from "fs";
 import path from "path";
 import { Readable } from "stream";
 import { spawn } from "child_process";
 
 
-async function downloadFromS3(bucket : string , key : string , destination : string ){
-    const command = new GetObjectCommand({Bucket : bucket , Key : key});
+async function downloadFromS3(bucket: string, key: string, destination: string) {
+    const command = new GetObjectCommand({ Bucket: bucket, Key: key });
     const response = await s3Client.send(command)
 
-    const stream  = response.Body as Readable;
-    return new Promise((resolve , reject) => {
+    const stream = response.Body as Readable;
+    return new Promise((resolve, reject) => {
         const fileWriter = fs.createWriteStream(destination);
         stream.pipe(fileWriter);
-        fileWriter.on('finish' , resolve) ; 
-        fileWriter.off('error' , reject);
+        fileWriter.on('finish', resolve);
+        fileWriter.off('error', reject);
     })
 
 }
 
 
 
-async function transcodingRawS3Video(inputPath : string , outputDir : string){
+async function transcodingRawS3Video(inputPath: string, outputDir: string) {
 
-  return new Promise((resolve , reject) => {
-       const ffmpeg = spawn('ffmpeg', [
-    '-i', inputPath,
+    return new Promise((resolve, reject) => {
+        const ffmpeg = spawn('ffmpeg', [
+            '-i', inputPath,
 
-    '-filter_complex',
-    '[0:v]split=5[v1][v2][v3][v4][v5];' +
-    '[v1]scale=-2:1080[v1out];' +
-    '[v2]scale=-2:720[v2out];' +
-    '[v3]scale=-2:480[v3out];' +
-    '[v4]scale=-2:360[v4out];' +
-    '[v5]scale=-2:240[v5out]',
+            '-filter_complex',
+            '[0:v]split=5[v1][v2][v3][v4][v5];' +
+            '[v1]scale=-2:1080[v1out];' +
+            '[v2]scale=-2:720[v2out];' +
+            '[v3]scale=-2:480[v3out];' +
+            '[v4]scale=-2:360[v4out];' +
+            '[v5]scale=-2:240[v5out]',
 
-    '-map', '[v1out]', '-c:v:0', 'libx264', '-b:v:0', '5000k',
-    '-map', '[v2out]', '-c:v:1', 'libx264', '-b:v:1', '2800k',
-    '-map', '[v3out]', '-c:v:2', 'libx264', '-b:v:2', '1400k',
-    '-map', '[v4out]', '-c:v:3', 'libx264', '-b:v:3', '1000k',
-    '-map', '[v5out]', '-c:v:4', 'libx264', '-b:v:4', '800k',
+            '-map', '[v1out]', '-c:v:0', 'libx264', '-b:v:0', '5000k',
+            '-map', '[v2out]', '-c:v:1', 'libx264', '-b:v:1', '2800k',
+            '-map', '[v3out]', '-c:v:2', 'libx264', '-b:v:2', '1400k',
+            '-map', '[v4out]', '-c:v:3', 'libx264', '-b:v:3', '1000k',
+            '-map', '[v5out]', '-c:v:4', 'libx264', '-b:v:4', '800k',
 
-     '-map' , '0:a', '-c:a', 'aac', '-b:a', '128k',
-      
-      '-f', 'hls',
-      '-hls_time', '10',
-      '-hls_playlist_type', 'vod',
-      '-master_pl_name', 'master.m3u8',
-      '-hls_segment_filename', `${outputDir}/v%v/segment_%03d.ts`,
-      `${outputDir}/v%v/index.m3u8`,
-  ]);
-    ffmpeg.on('close'  , (code) => {
-      if(code === 0 ) resolve(null);
-      else reject(`ffmpeg failed withc code${code}`)
+            '-map', '0:a', '-c:a', 'aac', '-b:a', '128k',
+
+            '-f', 'hls',
+            '-hls_time', '10',
+            '-hls_playlist_type', 'vod',
+            '-master_pl_name', 'master.m3u8',
+            '-var_stream_map', 'v:0,a:0 v:1,a:0 v:2,a:0 v:3,a:0 v:4,a:0',
+            '-hls_segment_filename', `${outputDir}/v%v/segment_%03d.ts`,
+            `${outputDir}/v%v/index.m3u8`,
+        ]);
+        ffmpeg.on('close', (code) => {
+            if (code === 0) resolve(null);
+            else reject(`ffmpeg failed withc code${code}`)
+        })
     })
-  })
 }
 
 
@@ -67,16 +68,16 @@ async function uploadFolderToS3(localDirPath: string, s3Bucket: string, s3Prefix
         const s3Key = path.join(s3Prefix, item);
 
         if (fs.statSync(localPath).isDirectory()) {
-            
+
             await uploadFolderToS3(localPath, s3Bucket, s3Key);
         } else {
-            
+
             const fileStream = fs.createReadStream(localPath);
             await s3Client.send(new PutObjectCommand({
                 Bucket: s3Bucket,
                 Key: s3Key,
                 Body: fileStream,
-                
+
                 ContentType: s3Key.endsWith('.m3u8') ? 'application/x-mpegURL' : 'video/MP2T'
             }));
         }
@@ -87,30 +88,34 @@ async function uploadFolderToS3(localDirPath: string, s3Bucket: string, s3Prefix
 async function run() {
     const BUCKET = process.env.S3_BUCKET!;
     const KEY = process.env.S3_KEY!;
-    const DEST_BUCKET = process.env.DEST_BUCKET!; 
-    
+    const DEST_BUCKET = process.env.DEST_BUCKET!;
+
     const inputPath = '/tmp/input_video.mp4';
     const outputDir = '/tmp/output';
 
     try {
-        
+        // Create output directory and variant subdirectories for HLS segments
+        fs.mkdirSync(outputDir, { recursive: true });
+        for (let i = 0; i < 5; i++) {
+            fs.mkdirSync(`${outputDir}/v${i}`, { recursive: true });
+        }
+
         console.log("Stage 1: Downloading...");
         await downloadFromS3(BUCKET, KEY, inputPath);
 
-   
         console.log("Stage 2: Transcoding...");
         await transcodingRawS3Video(inputPath, outputDir);
 
-       
+
         console.log("Stage 3: Uploading to new bucket...");
-        
-        const folderName = path.parse(KEY).name; 
+
+        const folderName = path.parse(KEY).name;
         await uploadFolderToS3(outputDir, DEST_BUCKET, `processed/${folderName}`);
 
         console.log("SUCCESS: Video is now live!");
     } catch (err) {
         console.error("FAILED:", err);
-        process.exit(1); 
+        process.exit(1);
     }
 }
 
