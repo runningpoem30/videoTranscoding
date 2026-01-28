602557a79d1951d4333a391cc7e14e31a7173dc1
runningpoem30 <aryaanandpathak30@gmail.com>
2026-01-28 20:18:36 +0530

    added the upload part

--- server/src/handler/worker.ts
+++ server/src/handler/worker.ts
@@ -23,18 +23,55 @@ async function downloadFromS3(bucket : string , key : string , destination : str
 
 
 async function transcodingRawS3Video(inputPath : string , outputDir : string){
-  const ffmpeg = spawn('ffmpeg' , [
-    'i' , inputPath , 
-    '-filter_complex' , '[0:v]split=5[v1][v2][v3][v4][v5]; [v1]scale=-2:1080[vout]; [v2]scale=-2:720[vout]; [v3]scale=-2:480[vout]; [v4]scale=-2:360[vout]; [v5]scale=-2:240'
+   const ffmpeg = spawn('ffmpeg', [
+    '-i', inputPath,
 
-  ])
+    '-filter_complex',
+    '[0:v]split=5[v1][v2][v3][v4][v5];' +
+    '[v1]scale=-2:1080[v1out];' +
+    '[v2]scale=-2:720[v2out];' +
+    '[v3]scale=-2:480[v3out];' +
+    '[v4]scale=-2:360[v4out];' +
+    '[v5]scale=-2:240[v5out]',
+
+    '-map', '[v1out]', '-c:v:0', 'libx264', '-b:v:0', '5000k',
+    '-map', '[v2out]', '-c:v:1', 'libx264', '-b:v:1', '2800k',
+    '-map', '[v3out]', '-c:v:2', 'libx264', '-b:v:2', '1400k',
+    '-map', '[v4out]', '-c:v:3', 'libx264', '-b:v:3', '1000k',
+    '-map', '[v5out]', '-c:v:4', 'libx264', '-b:v:4', '800k',
+
+     '-map' , '0:a', '-c:a', 'aac', '-b:a', '128k',
+      
+      '-f', 'hls',
+      '-hls_time', '10',
+      '-hls_playlist_type', 'vod',
+      '-master_pl_name', 'master.m3u8',
+      '-hls_segment_filename', `${outputDir}/v%v/segment_%03d.ts`,
+      `${outputDir}/v%v/index.m3u8`,
+  ]);
 }
 
 
+async function uploadFolderToS3(localDirPath: string, s3Bucket: string, s3Prefix: string) {
+    const items = fs.readdirSync(localDirPath);
 
-async function uploadToS3(bucket : string , key : string){
+    for (const item of items) {
+        const localPath = path.join(localDirPath, item);
+        const s3Key = path.join(s3Prefix, item);
 
+        if (fs.statSync(localPath).isDirectory()) {
+            
+            await uploadFolderToS3(localPath, s3Bucket, s3Key);
+        } else {
+            
+            const fileStream = fs.createReadStream(localPath);
+            await s3Client.send(new PutObjectCommand({
+                Bucket: s3Bucket,
+                Key: s3Key,
+                Body: fileStream,
+                
+                ContentType: s3Key.endsWith('.m3u8') ? 'application/x-mpegURL' : 'video/MP2T'
+            }));
+        }
+    }
 }
-
-
-//m3u8 is nothing but a text file , it gives instruction to the browser and video plaer what files to download and in what order nothing else
